68c59d4b6fa19fbb1d74204dfbaa3a4fd45a7d6b
Xavanion <barrettbrown2012@gmail.com>
2025-03-03 19:17:35 -0600

    Changed message datatype

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,9 +2,11 @@ import './styles/Home.css'
 
 function Home(){
     return (
-        <h1 className='home'>
-            Home
-        </h1>
+        <>
+            <h1 className='home'>
+                Welcome
+            </h1>
+        </>
     )
 }
 
